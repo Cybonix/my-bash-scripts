fe890c9d1c4e87622cccb9ba06bb245968070c9a
Joseph Mark Orimoloye <cybonix@gmail.com>
2023-06-10 01:53:48 -0400

    Update wdw.sh
    In this updated version of the script, the process_login function takes an extra argument that indicates whether the user is currently logged in. If this argument is 1, the function calculates the duration of the user's session and includes it in the output. 
    
    The date command is used to convert the login time to a Unix timestamp, and the difference between this timestamp and the current time is calculated to determine the duration of the session. 
    
    The duration is then formatted as hours, minutes, and seconds for display.

--- wdw.sh
+++ wdw.sh
@@ -12,35 +12,59 @@ real_users=$(awk -F: '$3 >= 1000 && $3 <= 60000 {print $1}' /etc/passwd)
 # Add root user to the list
 real_users=$(echo -e "$real_users\nroot")
 
-# Get the list of last logged-in instances
-logins=$(last -F | head -n -2)
+# Get the list of current logged-in users
+current_logins=$(who -u)
 
-# Traverse through each login
-while IFS= read -r line; do
+# Function to process each user login
+process_login() {
+    local line="$1"
+    local current="$2"
+
     # Extract the username and login time
     user=$(echo "$line" | awk '{print $1}')
-    login_time=$(echo "$line" | awk '{$1=$2=$3=""; print $0}')
+    login_time=$(echo "$line" | awk '{$1=$2=$3=$4=$5=""; print $0}')
 
     # Check if the user is a real user or root
     if echo "$real_users" | grep -q "^$user$"; then
         # Print the user and login time
-        echo "$user - Logged in at: $login_time"
+        if [ "$current" -eq 1 ]; then
+            # For currently logged in users, calculate how long they've been logged in
+            login_timestamp=$(date --date="$login_time" +%s)
+            current_timestamp=$(date +%s)
+            duration=$((current_timestamp - login_timestamp))
+            echo "$user - Logged in at: $login_time - Logged in for: $((duration / 3600))h $(((duration / 60) % 60))m $((duration % 60))s"
+        else
+            echo "$user - Logged in at: $login_time"
+        fi
 
         # Check if the history file exists and is readable
         history_file="/home/$user/.bash_history"
-        if [ ! -f "$history_file" ]; then
-            continue
+        if [ -f "$history_file" ]; then
+            # Get commands that likely involve a file
+            file_commands=$(egrep -i '^(vi|nano|cat|less|more|tail|head|touch|rm|cp|mv|mkdir|rmdir|cd) ' "$history_file")
+
+            # Print each command in a tree-like structure
+            while IFS= read -r command; do
+                echo "|-- $command"
+            done <<< "$file_commands"
         fi
-
-        # Get commands that likely involve a file
-        file_commands=$(egrep -i '^(vi|nano|cat|less|more|tail|head|touch|rm|cp|mv|mkdir|rmdir|cd) ' "$history_file")
-
-        # Print each command in a tree-like structure
-        while IFS= read -r command; do
-            echo "|-- $command"
-        done <<< "$file_commands"
 
         # Add extra line for clarity
         echo
     fi
-done <<< "$logins"
+}
+
+# Print information about currently logged in users
+echo "Currently logged in users:"
+while IFS= read -r line; do
+    process_login "$line" 1
+done <<< "$current_logins"
+
+# Get the list of last logged-in instances
+last_logins=$(last -F | head -n -2)
+
+# Print information about last logged in users
+echo "Last logged in users:"
+while IFS= read -r line; do
+    process_login "$line" 0
+done <<< "$last_logins"
