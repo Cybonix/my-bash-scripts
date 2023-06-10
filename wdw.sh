0190313736572f14c97408a714d80634e546fbe6
Joseph Mark Orimoloye <cybonix@gmail.com>
2023-06-10 01:04:54 -0400

    Update wdw.sh
    System users are filtered out, so only real users are listed.  If the history file for a user isn't accessible, the script simply shows the user's login time and does not display the "No accessible history file" message.

--- wdw.sh
+++ wdw.sh
@@ -6,7 +6,10 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Get the list of last logged-in users
+# Get the list of real users
+real_users=$(awk -F: '$3 >= 1000 && $3 <= 60000 {print $1}' /etc/passwd)
+
+# Get the list of last logged-in instances
 logins=$(last -F | head -n -2)
 
 # Traverse through each login
@@ -15,24 +18,26 @@ while IFS= read -r line; do
     user=$(echo "$line" | awk '{print $1}')
     login_time=$(echo "$line" | awk '{$1=$2=$3=""; print $0}')
 
-    # Print the user and login time
-    echo "$user - Logged in at: $login_time"
-    
-    # Check if the history file exists and is readable
-    history_file="/home/$user/.bash_history"
-    if [ ! -f "$history_file" ]; then
-        echo "|-- No accessible history file"
-        continue
+    # Check if the user is a real user
+    if echo "$real_users" | grep -q "^$user$"; then
+        # Print the user and login time
+        echo "$user - Logged in at: $login_time"
+
+        # Check if the history file exists and is readable
+        history_file="/home/$user/.bash_history"
+        if [ ! -f "$history_file" ]; then
+            continue
+        fi
+
+        # Get commands that likely involve a file
+        file_commands=$(egrep -i '^(vi|nano|cat|less|more|tail|head|touch|rm|cp|mv|mkdir|rmdir|cd) ' "$history_file")
+
+        # Print each command in a tree-like structure
+        while IFS= read -r command; do
+            echo "|-- $command"
+        done <<< "$file_commands"
+
+        # Add extra line for clarity
+        echo
     fi
-
-    # Get commands that likely involve a file
-    file_commands=$(egrep -i '^(vi|nano|cat|less|more|tail|head|touch|rm|cp|mv|mkdir|rmdir|cd) ' "$history_file")
-
-    # Print each command in a tree-like structure
-    while IFS= read -r command; do
-        echo "|-- $command"
-    done <<< "$file_commands"
-
-    # Add extra line for clarity
-    echo
 done <<< "$logins"
