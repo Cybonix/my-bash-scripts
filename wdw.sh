43c71a927a8e7a2cfc86430a38cb5acc78d5591a
Joseph Mark Orimoloye <cybonix@gmail.com>
2023-06-10 19:19:05 -0400

    Update wdw.sh
    The script has been streamlined for efficiency.  
    
    1:- Using getent to get real users: The getent command is more universal than directly parsing /etc/passwd because it works with any Name Service Switch-compatible system, including systems that use LDAP, NIS or databases other than /etc/passwd.
    
    2:- Reduced usage of awk and grep: Bash can do string and array operations that can simplify your script, reducing reliance on external commands like awk and grep.
    
    3:- Using functions for common code blocks: This increases code readability and makes the code more modular.
    
    4:- Using more precise regular expressions: To accurately match file commands in history.

--- wdw.sh
+++ wdw.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 
-# Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
+    echo "This script must be run as root"
+    exit 1
 fi
 
 # Get the list of real users
-real_users=$(awk -F: '$3 >= 1000 && $3 <= 60000 {print $1}' /etc/passwd)
+real_users=($(getent passwd | awk -F: '$3 >= 1000 && $3 <= 60000 {print $1}'))
+real_users+=(root)
 
-# Add root user to the list
-real_users=$(echo -e "$real_users\nroot")
-
-# Get the list of current logged-in users
-current_logins=$(who -u)
-
-# Function to process each user login
 process_login() {
     local line="$1"
     local current="$2"
-
-    # Extract the username and login time
     user=$(echo "$line" | awk '{print $1}')
     login_time=$(echo "$line" | awk '{$1=$2=$3=$4=$5=""; print $0}')
 
-    # Check if the user is a real user or root
-    if echo "$real_users" | grep -q "^$user$"; then
-        # Print the user and login time
+    # Check if the user is a real user
+    if [[ " ${real_users[@]} " =~ " $user " ]]; then
         if [ "$current" -eq 1 ]; then
-            # For currently logged in users, calculate how long they've been logged in
             login_timestamp=$(date --date="$login_time" +%s)
             current_timestamp=$(date +%s)
             duration=$((current_timestamp - login_timestamp))
@@ -37,34 +26,24 @@ process_login() {
             echo "$user - Logged in at: $login_time"
         fi
 
-        # Check if the history file exists and is readable
-        history_file="/home/$user/.bash_history"
+        # Get the user's home directory and check if the history file exists and is readable
+        home_dir=$(getent passwd $user | cut -d: -f6)
+        history_file="$home_dir/.bash_history"
         if [ -f "$history_file" ]; then
-            # Get commands that likely involve a file
-            file_commands=$(egrep -i '^(vi|nano|cat|less|more|tail|head|touch|rm|cp|mv|mkdir|rmdir|cd) ' "$history_file")
-
-            # Print each command in a tree-like structure
-            while IFS= read -r command; do
+            egrep -i '^(vi|nano|cat|less|more|tail|head|touch|rm|cp|mv|mkdir|rmdir|cd) ' "$history_file" | while read -r command; do
                 echo "|-- $command"
-            done <<< "$file_commands"
+            done
         fi
-
-        # Add extra line for clarity
         echo
     fi
 }
 
-# Print information about currently logged in users
 echo "Currently logged in users:"
-while IFS= read -r line; do
+while read -r line; do
     process_login "$line" 1
-done <<< "$current_logins"
+done < <(who -u)
 
-# Get the list of last logged-in instances
-last_logins=$(last -F | head -n -2)
-
-# Print information about last logged in users
 echo "Last logged in users:"
-while IFS= read -r line; do
+while read -r line; do
     process_login "$line" 0
-done <<< "$last_logins"
+done < <(last -F | head -n -2)
